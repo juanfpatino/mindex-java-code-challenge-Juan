c4fb638dfe5ee37d43896746b0fa2755ca6327f6
Juan Patino <juan9patino9@gmail.com>
2024-02-27 16:20:35 -0500

    ReportingStructure class and Test class stubbed out

---
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -0,0 +1,41 @@
+package com.mindex.challenge.data;
+
+/**
+ * Represents the reporting structure of an employee.
+ * author: Juan Francisco Patino
+ */
+public class ReportingStructure {
+
+    private Employee employee;
+    private Integer numberOfReports;
+
+    /**
+     * Constructor
+     */
+    public ReportingStructure() {
+    }
+
+    // Getters and Setters
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    // DFS to count the number of reports
+    public int getNumberOfReports() {
+        if(numberOfReports == null){
+            return getNumberOfReports(this.employee);
+        }
+        return numberOfReports;
+    }
+
+    public int getNumberOfReports(Employee employee) {
+        return numberOfReports;
+    }
+
+
+}
---
+++ src/test/java/com/mindex/challenge/service/impl/ReportingStructureTest.java
@@ -0,0 +1,23 @@
+package com.mindex.challenge.service.impl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Test class for Reporting Structure Class
+ * @Author Juan Francisco Patino
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportingStructureTest {
+
+    @Test
+    public void johnLennonTest(){
+
+        // todo assert Employee John Lennon has 4 reports
+
+    }
+
+}
