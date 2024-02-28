1ca730b4ee163869b12ccd70d771b312dc5ff814
juanfpatino <jfp6815@rit.edu>
2024-02-27 20:18:29 -0500

    Fixed decorators in ReportingStructureServiceImpl.java

--- src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class ReportingStructureController {
 
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
